e6cd6e86fcabb6ca372d96654402037325eb0d83
Martijn2001 <martijnuva@gmail.com>
2023-05-04 16:33:56 +0200

    added support for multiple graph types

---
+++ app/static/js/logic-modules/data-selection-glm.js
@@ -0,0 +1,71 @@
+var current_variables_selected = -1;
+var line_graph = ['date', 'int'];
+var bar_graph = ['string'];
+var pie_chart = ['string'];
+
+export async function getResourceVariables(graph_type, resource_type, selected_variable) {
+    const response = await fetch('../../static/dicts/resource_variables.json');
+    const data = await response.json();
+    for (var resource in data) {
+        if (resource.toLowerCase() == resource_type.toLowerCase()) {
+            current_variables_selected++;
+            var variables = Object.entries(data[resource][0]);
+            var filtered_variables = filterVariables(variables, graph_type, selected_variable);
+            var interface_text = await writeInterfaceText(graph_type);
+            return [current_variables_selected, interface_text, filtered_variables];
+
+        }
+    }
+}
+
+function filterVariables(variables, graph_type, selected_variable) {
+    var possible_variables = [];
+    variables.forEach(variable => {
+        var variable_name = variable[0];
+        var variable_type = variable[1];
+        var allowed_variables;
+        if (graph_type == 'Line graph') {
+            allowed_variables = line_graph;
+        } else if (graph_type == 'Bar graph') {
+            allowed_variables = bar_graph;
+        } else if (graph_type == 'Pie chart') {
+            allowed_variables = pie_chart;
+        }
+
+        if (allowed_variables.includes(variable_type)) {
+            if (selected_variable !== undefined && selected_variable != variable_name) {
+                possible_variables.push(variable_name);
+            } else if (selected_variable == undefined) {
+                possible_variables.push(variable_name);
+            }
+        }
+    });
+
+    return possible_variables;
+}
+
+async function writeInterfaceText(graph_type) {
+    if (graph_type == 'Line graph') {
+        if (current_variables_selected == 0) {
+            return "Select the X variable for your line graph";
+        } else if (current_variables_selected >= 1) {
+            return "Select the Y variable for your line graph";
+        }
+    } else if (graph_type == 'Bar graph') {
+        if (current_variables_selected == 0) {
+            return "Select the variable you want to create a bar graph with";
+        } else {
+            return 1;
+        }
+    } else if (graph_type == 'Pie chart') {
+        if (current_variables_selected == 0) {
+            return "Select the variable you want to create a pie chart with";
+        } else {
+            return 1;
+        }
+    }
+}
+
+export function resetCurrentVariablesSelected() {
+    current_variables_selected = -1;
+}
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,23 +1,27 @@
 // Custom Javascript
+import { getResourceVariables, resetCurrentVariablesSelected } from '/../static/js/logic-modules/data-selection-glm.js';
+
 var graph_location
 var graph_type
 var resource_type
 
-$("#add-graph").click(function() {
-    $('[id^="graph-location"]').bind("click", function() {
-        $('[id^="graph-location"]').off("click"); 
+
+$("#add-graph").click(function () {
+    $('[id^="graph-location"]').bind("click", function () {
+        $('[id^="graph-location"]').off("click");
         graph_location = $(this);
         $(graph_location).addClass("bg-secondary").removeClass("bg-light");
+        $("#add-graph").nextAll().empty();
         showSelectGraph();
     })
 })
 
 function showSelectGraph() {
-    $("#select-graph").html(`
+    $("#select-graph-div").html(`
         <br>
         <p> Select the type of graph you want to create </p>
         <select class="form-select" id="select-graph-type" name=select-graph-type" value="select-graph-type">
-        <option>Graph type</option>
+        <option style="display: none">Graph type</option>
         <option value="Bar graph">Bar graph</option>
         <option value="Line graph">Line graph</option>
         <option value="Pie chart">Pie chart</option>
@@ -25,25 +29,60 @@ function showSelectGraph() {
     `);
 }
 
-$(document.body).on('change',"#select-graph-type",function () {
+$(document.body).off('change', '#select-graph-type').on('change', "#select-graph-type", function () {
+    $("#select-resource-div").empty();
+    $("#select-variabele-div").empty();
+    resetCurrentVariablesSelected();
     graph_type = $("#select-graph-type option:selected").val();
     showSelectResource();
 });
 
 function showSelectResource() {
-    $("#select-resource").html(`
+    $("#select-resource-div").html(`
         <br>
         <p> Now select the a resource for which you want to create a graph </p>
         <select class="form-select" id="select-resource" name="select-resource" value="select-resource">
-        <option>Resource</option>
-        <option value="Patient">Patient</option>
-        <option value="Medication">Medication</option>
+        <option style="display: none">Resource</option>
+        <option value="Careplans">Careplans</option>
         <option value="Allergies">Allergies</option>
         </select>
     `);
 }
 
-$(document.body).on('change',"#select-resource",function () {
+$(document.body).off('change', '#select-resource').on('change', "#select-resource", async function () {
+    $(this).off('change');
+    $("#select-variabele-div").empty();
+    resetCurrentVariablesSelected();
     resource_type = $("#select-resource option:selected").val();
-    $(graph_location).html("Hier komt nu een " + graph_type + " van data " + resource_type);
+
+    async function getPossibleVariables(selected_variable) {
+        var [current_number_of_variables_selected, interface_text, possible_variables] = await getResourceVariables(graph_type, resource_type, selected_variable);
+        if (interface_text != 1) {
+            showSelectVariable(current_number_of_variables_selected, interface_text, possible_variables);
+        }
+        
+    }
+
+    await getPossibleVariables();
+
+    $(document.body).off('change', '#select-variable-0').on('change', "#select-variable-0", async function () {
+        $(this).off('change');
+        $(this).nextAll().remove();
+        var selected_variable = $("#select-variable-0 option:selected").val();
+        await getPossibleVariables(selected_variable);
+    })
 });
+
+function showSelectVariable(current_number_of_variables_selected, interface_text, possible_variables) {
+    var select_string = `
+        <br>
+        <p> ` + interface_text + ` </p>
+        <select class="form-select" id="select-variable-` + current_number_of_variables_selected + `" name="select-variabele-` + current_number_of_variables_selected + `" value="select-variabele-` + current_number_of_variables_selected + `">
+        <option style="display: none">Variabele</option>
+        `;
+    possible_variables.forEach(variabele => {
+        select_string += '<option value="' + variabele + '">' + variabele + '</option>'
+    });
+    select_string += "</select >"
+    $("#select-variabele-div").append(select_string);
+}
