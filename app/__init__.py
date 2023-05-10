3e038a24578fd8886f2fa83a72787262ac05bcc2
Dilara <dilara.tank1@student.uva.nl>
2023-05-10 13:22:54 +0200

    first try

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,54 @@
-from flask import Flask, render_template
-from flask.helpers import request
+from flask import Flask, config, render_template, request
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
 from flask_bootstrap import Bootstrap5
-from config import Config
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+# configs and received from frontend
+data_path = '/home/dilaratank/Desktop/FHIRdevdays/synthea/synthea_output/csv/' # this will probably be difference once we receive data from the database
+graph_type = 'barchart'
+resource = 'patients'
+data_element_x = 'GENDER'
+data_element_y = 'HEALTHCARE_EXPENSES'
 
-    bootstrap = Bootstrap5(app)
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 
-    # Initialize Flask extensions here
+@app.route('/callback', methods=['POST', 'GET'])
+def cb():
+    return gm(data_path, resource, graph_type, data_element_x, data_element_y)
+   
+@app.route('/')
+def index():
+    return render_template('index.html',  graphJSON=gm(data_path, resource, graph_type, data_element_x, data_element_y))
 
-    # Register blueprints here
+def gm(data_path, resource, graph_type, data_element_x, data_element_y):
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+    # read data and define dataframes
+    data = pd.read_csv(data_path + resource + '.csv')
+    x_var = data[data_element_x]
+    if graph_type != 'piechart':
+        y_var = data[data_element_y]
 
-    return app
+    # Define the chart
+    if graph_type == 'linechart':
+        fig = go.Figure(data=[go.Scatter(x=x_var, y=y_var, mode='lines')])
+        fig.update_layout(title='Line Chart', xaxis_title=data_element_x, yaxis_title=data_element_y)
+    elif graph_type == 'barchart':
+        df = data.groupby(data_element_x)[data_element_y].sum().reset_index()
+        fig = go.Figure(data=[go.Bar(x=df[data_element_x], y=df[data_element_y])])
+        fig.update_layout(title='Bar Chart', xaxis_title=data_element_x, yaxis_title=data_element_y)
+    elif graph_type == 'piechart':
+        df = data.groupby(data_element_x).size().rename('Value').reset_index()
+        fig = go.Figure(data=[go.Pie(labels=df[data_element_x], values=df['Value'])])
+        fig.update_layout(title='Pie Chart', xaxis_title=data_element_x, yaxis_title=data_element_y)
+    elif graph_type == 'scatterplot':
+        fig = go.Figure(data=[go.Scatter(x=x_var, y=y_var, mode='markers')])
+        fig.update_layout(title='Line Chart', xaxis_title=data_element_x, yaxis_title=data_element_y)
+    
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    print(fig.data[0])
+    
+    return graphJSON
