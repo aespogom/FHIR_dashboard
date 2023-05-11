ad81afd44e5ffb9c67125636f0c0da11e00dc861
aespogom <100383119@alumnos.uc3m.es>
2023-05-11 18:54:06 +0200

    General query search resource and filter datetime

--- src/backend/queries.py
+++ src/backend/queries.py
@@ -2,35 +2,59 @@
     This module contains all the available queries for the Firely server
 '''
 
-from utils import smart
-from fhirclient.models import *
+from datetime import datetime
+import os
+from typing import Union
+from dotenv import load_dotenv
+import requests
+import utils
 
+load_dotenv()
 
-# Define the input parameters from frontend/ploty
-resource = 'Observation'
-x = 'category'
-y = 'valueQuantity'
-date_attribute = 'effectiveDateTime'
-date_from='2023-05-01T00:00:00Z'
-date_to='2023-05-10T00:00:00Z'
+def query_firely_server(resource: str,
+                        x: str,
+                        y: str,
+                        date_col: Union[str, None],
+                        date_from: Union[datetime, None],
+                        date_to: Union[datetime, None]):
+    # Initialize query
+    query = os.getenv("URL_SERVER")
 
-def query_firely_server(resource, x, y, date_col, date_from, date_to):
+    # Access resource
+    resource = resource.lower().capitalize()
+    query = query + resource
+
     # Construct the datetime query
-    search_params = {
-        date_attribute: {"$gte": date_from},
-        date_attribute: {"$lte": date_to}
-    }
-
-    # Create a search query for the specified resource type
-    query = getattr(smart, resource).where(params=search_params)
+    if date_from and date_col:
+        query = query + '?'+date_col+'=$gte='+date_from.strftime("%Y-%m-%d")
+    if date_to and date_col:
+        query = query + '?'+date_col+'=$lte='+date_to.strftime("%Y-%m-%d")
 
     # Perform the search
-    result = query.perform_resources()
+    result = requests.get(query)
 
     #TODO model
+    if result.status_code and result.status_code==200:
+        list_registries = []
+        bundle = result.json()
+        # Skip last element which is always the OperationOutcome
+        for entry in list(bundle['entry'])[:-1]:
+            registry = eval("utils."+resource.lower()+"."+resource+"(entry['resource'])")
+            list_registries.append(registry)
+    
+        return list_registries
+
     return result
 
-query_firely_server(resource, x, y, date_attribute, date_from, date_to)
+# # Define the input parameters from frontend/ploty
+# resource = 'Observation'
+# x = 'category'
+# y = 'valueQuantity'
+# date_attribute = 'effectiveDateTime'
+# date_from=datetime(2023,5,1,0,0,0)
+# date_to=datetime(2023,5,10,0,0,0)
+# r = query_firely_server(resource, x, y, date_attribute, date_from, date_to)
+# print(r)
 
 
 
